cf66905febd1d1f27ee36ea0abdc7f2a7ba29b2d
Myesha <maisha.mahjabin27@gmail.com>
2024-05-12 17:09:32 -0400

    updated chat.c

--- chat.c
+++ chat.c
@@ -1,11 +1,12 @@
 #include <gtk/gtk.h>
-#include <glib/gunicode.h> /* for utf8 strlen */
+#include <glib/gunicode.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
 #include <openssl/sha.h>
 #include <openssl/evp.h>
 #include <openssl/hmac.h>
+#include <openssl/rand.h>
 #include <getopt.h>
 #include "dh.h"
 #include "keys.h"
@@ -14,7 +15,6 @@
 #define PATH_MAX 1024
 #endif
 
-// not available by default on all systems
 #ifndef HOST_NAME_MAX
 #define HOST_NAME_MAX 255
 #endif
@@ -24,18 +24,21 @@ static GtkTextBuffer *mbuf; /* message buffer */
 static GtkTextView *tview;	/* view for transcript */
 static GtkTextMark *mark;	/* used for scrolling to end of transcript, etc */
 
-static pthread_t trecv; /* wait for incoming messagess and post to queue */
+static pthread_t trecv; /* wait for incoming messages and post to queue */
 void *recvMsg(void *);	/* for trecv */
 
-#define max(a, b) \
-	({ typeof(a) _a = a;    \
-	 typeof(b) _b = b;    \
-	 _a > _b ? _a : _b; })
+#define max(a, b)          \
+	({                     \
+		typeof(a) _a = a;  \
+		typeof(b) _b = b;  \
+		_a > _b ? _a : _b; \
+	})
 
 /* network stuff... */
 
 static int listensock, sockfd;
 static int isclient = 1;
+static unsigned char sharedSecret[SHA256_DIGEST_LENGTH];
 
 static void error(const char *msg)
 {
@@ -49,7 +52,6 @@ int initServerNet(int port)
 	struct sockaddr_in serv_addr;
 	listensock = socket(AF_INET, SOCK_STREAM, 0);
 	setsockopt(listensock, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse));
-	/* NOTE: might not need the above if you make sure the client closes first */
 	if (listensock < 0)
 		error("ERROR opening socket");
 	bzero((char *)&serv_addr, sizeof(serv_addr));
@@ -67,7 +69,6 @@ int initServerNet(int port)
 		error("error on accept");
 	close(listensock);
 	fprintf(stderr, "connection made, starting session...\n");
-	/* at this point, should be able to send/recv on sockfd */
 	return 0;
 }
 
@@ -90,7 +91,6 @@ static int initClientNet(char *hostname, int port)
 	serv_addr.sin_port = htons(port);
 	if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
 		error("ERROR connecting");
-	/* at this point, should be able to send/recv on sockfd */
 	return 0;
 }
 
@@ -107,7 +107,133 @@ static int shutdownNetwork()
 	return 0;
 }
 
-/* end network stuff. */
+static void performKeyExchange()
+{
+	if (isclient)
+	{
+		// Generate and exchange ephemeral keys with the server
+		dhKey clientKey, serverKey;
+		initKey(&clientKey);
+		initKey(&serverKey);
+
+		// Generate client's ephemeral key pair
+		mpz_t x, X;
+		mpz_init(x);
+		mpz_init(X);
+		dhGen(x, X);
+
+		// Send client's public key to the server
+		writeDH("client_key", &clientKey);
+
+		// Receive server's public key from the client
+		readDH("server_key", &serverKey);
+
+		// Calculate shared secret
+		dhFinal(x, X, serverKey.PK, sharedSecret, SHA256_DIGEST_LENGTH);
+
+		// Cleanup
+		mpz_clear(x);
+		mpz_clear(X);
+		shredKey(&clientKey);
+		shredKey(&serverKey);
+	}
+	else
+	{
+		// Generate and exchange ephemeral keys with the client
+		dhKey clientKey, serverKey;
+		initKey(&clientKey);
+		initKey(&serverKey);
+
+		// Receive client's public key from the client
+		readDH("client_key", &clientKey);
+
+		// Generate server's ephemeral key pair
+		mpz_t y, Y;
+		mpz_init(y);
+		mpz_init(Y);
+		dhGen(y, Y);
+
+		// Send server's public key to the client
+		writeDH("server_key", &serverKey);
+
+		// Calculate shared secret
+		dhFinal(serverKey.SK, serverKey.PK, clientKey.PK, sharedSecret, SHA256_DIGEST_LENGTH);
+
+		// Cleanup
+		mpz_clear(y);
+		mpz_clear(Y);
+		shredKey(&clientKey);
+		shredKey(&serverKey);
+	}
+}
+
+static void encryptMessage(char *message, size_t len)
+{
+	// Encrypt message using AES encryption
+	EVP_CIPHER_CTX *ctx;
+	unsigned char iv[16];
+	unsigned char ciphertext[len + EVP_MAX_BLOCK_LENGTH];
+	int ciphertext_len, len_tmp;
+
+	// Generate random IV
+	RAND_bytes(iv, sizeof(iv));
+
+	// Initialize encryption context
+	ctx = EVP_CIPHER_CTX_new();
+	EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, sharedSecret, iv);
+
+	// Encrypt message
+	EVP_EncryptUpdate(ctx, ciphertext, &ciphertext_len, (unsigned char *)message, len);
+	EVP_EncryptFinal_ex(ctx, ciphertext + ciphertext_len, &len_tmp);
+	ciphertext_len += len_tmp;
+
+	// Send IV and encrypted message
+	send(sockfd, iv, sizeof(iv), 0);
+	send(sockfd, ciphertext, ciphertext_len, 0);
+
+	EVP_CIPHER_CTX_free(ctx);
+}
+
+static void decryptMessage(unsigned char *iv, char *message, size_t len)
+{
+	// Decrypt message using AES decryption
+	EVP_CIPHER_CTX *ctx;
+	unsigned char plaintext[len + EVP_MAX_BLOCK_LENGTH];
+	int plaintext_len, len_tmp;
+
+	// Initialize decryption context
+	ctx = EVP_CIPHER_CTX_new();
+	EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, sharedSecret, iv);
+
+	// Decrypt message
+	EVP_DecryptUpdate(ctx, plaintext, &plaintext_len, (unsigned char *)message, len);
+	EVP_DecryptFinal_ex(ctx, plaintext + plaintext_len, &len_tmp);
+	plaintext_len += len_tmp;
+
+	// Print decrypted message
+	printf("Received message: %s\n", plaintext);
+
+	EVP_CIPHER_CTX_free(ctx);
+}
+
+static void authenticate()
+{
+	// Exchange encrypted test messages for mutual authentication
+	char *testMessage = "This is a test message for mutual authentication.";
+	size_t len = strlen(testMessage);
+
+	// Encrypt and send test message
+	encryptMessage(testMessage, len);
+
+	// Receive encrypted test message and IV
+	unsigned char iv[16];
+	recv(sockfd, iv, sizeof(iv), 0);
+	char encryptedMessage[len + EVP_MAX_BLOCK_LENGTH];
+	recv(sockfd, encryptedMessage, len + EVP_MAX_BLOCK_LENGTH, 0);
+
+	// Decrypt received message
+	decryptMessage(iv, encryptedMessage, len);
+}
 
 static const char *usage =
 	"Usage: %s [OPTIONS]...\n"
@@ -117,11 +243,6 @@ static const char *usage =
 	"   -p, --port    PORT  Listen or connect on PORT (defaults to 1337).\n"
 	"   -h, --help          show this message and exit.\n";
 
-/* Append message to transcript with optional styling.  NOTE: tagnames, if not
- * NULL, must have it's last pointer be NULL to denote its end.  We also require
- * that messsage is a NULL terminated string.  If ensurenewline is non-zero, then
- * a newline may be added at the end of the string (possibly overwriting the \0
- * char!) and the view will be scrolled to ensure the added line is visible.  */
 static void tsappend(char *message, char **tagnames, int ensurenewline)
 {
 	GtkTextIter t0;
@@ -132,7 +253,6 @@ static void tsappend(char *message, char **tagnames, int ensurenewline)
 	gtk_text_buffer_insert(tbuf, &t0, message, len);
 	GtkTextIter t1;
 	gtk_text_buffer_get_end_iter(tbuf, &t1);
-	/* Insertion of text may have invalidated t0, so recompute: */
 	t0 = t1;
 	gtk_text_iter_backward_chars(&t0, len);
 	if (tagnames)
@@ -151,25 +271,22 @@ static void tsappend(char *message, char **tagnames, int ensurenewline)
 	gtk_text_buffer_delete_mark(tbuf, mark);
 }
 
-static void sendMessage(GtkWidget *w /* <-- msg entry widget */, gpointer /* data */)
+static void sendMessage(GtkWidget *w, gpointer data)
 {
 	char *tags[2] = {"self", NULL};
 	tsappend("me: ", tags, 0);
-	GtkTextIter mstart; /* start of message pointer */
-	GtkTextIter mend;	/* end of message pointer */
+	GtkTextIter mstart;
+	GtkTextIter mend;
 	gtk_text_buffer_get_start_iter(mbuf, &mstart);
 	gtk_text_buffer_get_end_iter(mbuf, &mend);
 	char *message = gtk_text_buffer_get_text(mbuf, &mstart, &mend, 1);
 	size_t len = g_utf8_strlen(message, -1);
-	/* XXX we should probably do the actual network stuff in a different
-	 * thread and have it call this once the message is actually sent. */
 	ssize_t nbytes;
 	if ((nbytes = send(sockfd, message, len, 0)) == -1)
 		error("send failed");
 
 	tsappend(message, NULL, 1);
 	free(message);
-	/* clear message text and reset focus */
 	gtk_text_buffer_delete(mbuf, &mstart, &mend);
 	gtk_widget_grab_focus(w);
 }
@@ -185,6 +302,27 @@ static gboolean shownewmessage(gpointer msg)
 	return 0;
 }
 
+void *recvMsg(void *arg)
+{
+	size_t maxlen = 512;
+	char msg[maxlen + 2];
+	ssize_t nbytes;
+	while (1)
+	{
+		if ((nbytes = recv(sockfd, msg, maxlen, 0)) == -1)
+			error("recv failed");
+		if (nbytes == 0)
+			return 0;
+		char *m = malloc(maxlen + 2);
+		memcpy(m, msg, nbytes);
+		if (m[nbytes - 1] != '\n')
+			m[nbytes++] = '\n';
+		m[nbytes] = 0;
+		g_main_context_invoke(NULL, shownewmessage, (gpointer)m);
+	}
+	return 0;
+}
+
 int main(int argc, char *argv[])
 {
 	if (init("params") != 0)
@@ -192,14 +330,14 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "could not read DH params from file 'params'\n");
 		return 1;
 	}
-	// define long options
+
 	static struct option long_opts[] = {
 		{"connect", required_argument, 0, 'c'},
 		{"listen", no_argument, 0, 'l'},
 		{"port", required_argument, 0, 'p'},
 		{"help", no_argument, 0, 'h'},
 		{0, 0, 0, 0}};
-	// process options:
+
 	char c;
 	int opt_index = 0;
 	int port = 1337;
@@ -228,10 +366,7 @@ int main(int argc, char *argv[])
 			return 1;
 		}
 	}
-	/* NOTE: might want to start this after gtk is initialized so you can
-	 * show the messages in the main window instead of stderr/stdout.  If
-	 * you decide to give that a try, this might be of use:
-	 * https://docs.gtk.org/gtk4/func.is_initialized.html */
+
 	if (isclient)
 	{
 		initClientNet(hostname, port);
@@ -241,7 +376,6 @@ int main(int argc, char *argv[])
 		initServerNet(port);
 	}
 
-	/* setup GTK... */
 	GtkBuilder *builder;
 	GObject *window;
 	GObject *button;
@@ -273,12 +407,10 @@ int main(int argc, char *argv[])
 											  GTK_STYLE_PROVIDER(css),
 											  GTK_STYLE_PROVIDER_PRIORITY_USER);
 
-	/* setup styling tags for transcript text buffer */
 	gtk_text_buffer_create_tag(tbuf, "status", "foreground", "#657b83", "font", "italic", NULL);
 	gtk_text_buffer_create_tag(tbuf, "friend", "foreground", "#6c71c4", "font", "bold", NULL);
 	gtk_text_buffer_create_tag(tbuf, "self", "foreground", "#268bd2", "font", "bold", NULL);
 
-	/* start receiver thread: */
 	if (pthread_create(&trecv, 0, recvMsg, 0))
 	{
 		fprintf(stderr, "Failed to create update thread.\n");
@@ -287,32 +419,5 @@ int main(int argc, char *argv[])
 	gtk_main();
 
 	shutdownNetwork();
-	return 0;
-}
-
-/* thread function to listen for new messages and post them to the gtk
- * main loop for processing: */
-void *recvMsg(void *)
-{
-	size_t maxlen = 512;
-	char msg[maxlen + 2]; /* might add \n and \0 */
-	ssize_t nbytes;
-	while (1)
-	{
-		if ((nbytes = recv(sockfd, msg, maxlen, 0)) == -1)
-			error("recv failed");
-		if (nbytes == 0)
-		{
-			/* XXX maybe show in a status message that the other
-			 * side has disconnected. */
-			return 0;
-		}
-		char *m = malloc(maxlen + 2);
-		memcpy(m, msg, nbytes);
-		if (m[nbytes - 1] != '\n')
-			m[nbytes++] = '\n';
-		m[nbytes] = 0;
-		g_main_context_invoke(NULL, shownewmessage, (gpointer)m);
-	}
 	return 0;
 }
